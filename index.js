3b0dd29b47f377ded08e59f2017d23fa0dd6b238
Abhishek-Mohajan-projects <avimm079@gmail.com>
2023-10-09 12:25:56 +0600

    server side validation- express validator

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const { body, validationResult } = require('express-validator');
+const express = require("express");
+const { body, validationResult } = require("express-validator");
 const app = express();
-const userRouter = require("./routes/User")
+const userRouter = require("./routes/User");
 const PORT = 4000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/test", (req, res) => {
-    res.status(200).send("testing routes");
+  res.status(200).send("testing routes");
 });
 
 app.use("/api", userRouter);
 
 app.listen(PORT, () => {
-    console.log(`server is running at http://localhost:${PORT}`);
+  console.log(`server is running at http://localhost:${PORT}`);
 });
